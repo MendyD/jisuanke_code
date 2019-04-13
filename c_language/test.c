85ac216efd2011f55f20d757dfe1bc3f6ed970ba
“MendyD” <dolai_c@163.com>
2019-04-13 11:29:44 +0800

    Create test.c

---
+++ c_language/test.c
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: Mendy
+	> Mail: dolai_c@163.com
+	> Course: 
+	> Created Time: 六  1/12 00:36:22 2019
+ ************************************************************************/
+
+#include<stdio.h>
+union register{
+    struct {
+        unsigned char byte1;
+        unsigned char byte2;
+        unsigned char byte3;
+        unsigned char byte4;
+    } bytes;
+    unsigned int number;
+};
+int main() {
+    
+}
