ed732a3cc15e93e1d8970c34ee8d532993c72307
“MendyD” <dolai_c@163.com>
2019-04-13 11:16:40 +0800

    Create 14_3.c

---
+++ c_language/14_3.c
@@ -0,0 +1,32 @@
+/*************************************************************************
+	> File Name: 14_3.c
+	> Author: Mendy
+	> Mail: dolai_c@163.com
+	> Course: 
+	> Created Time: 三 12/26 10:59:35 2018
+ ************************************************************************/
+
+#include<stdio.h>
+#include <string.h>
+void harsh(char str[]){
+    int arr[32] = {0};
+    int bits[32];
+
+    int i, j;
+    int len = strlen(str);
+    for(i = 1; i <= len; i++){
+        arr[i % 32] += str[i - 1];
+
+    }
+    for(j = 0; j< 32; j++){
+        bits[j] = arr[31 - j] ^ (arr[j] << 1);
+        printf("%c", bits[j] % 85 + 34);
+    }
+    printf("\n");
+}
+int main() {
+	char str[505];
+    scanf("%s", str);
+    harsh(str);
+    return 0;
+}
